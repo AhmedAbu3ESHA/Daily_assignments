f049d21c742bd295adced215b70094e67ea859f5
AhmedAbu3esha <38920018+AhmedAbu3ESHA@users.noreply.github.com>
2024-01-18 12:54:16 +0200

    Add files via upload
    calculator use( HTML,CSS,JS)

---
+++ task2(17-1-2024)/calc js.js
@@ -0,0 +1,21 @@
+// This function clears all the values
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+ 
+// This function displays the values
+
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+ 
+// This function evaluates the expression and returns the result
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+    alert("this is"+q);
+}
+
+
