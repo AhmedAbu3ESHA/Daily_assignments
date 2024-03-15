be6ecb780726d064582b23922a283dbaf69614bc
AhmedAbu3esha <38920018+AhmedAbu3ESHA@users.noreply.github.com>
2024-03-15 15:58:45 +0200

    Delete design pattern/SOLID((S-srb,O-ocb) princibles) directory

--- design pattern/SOLID((S-srb,O-ocb) princibles)/src/services/IncomeCalculatorInterface.java
+++
@@ -1,7 +0,0 @@
-package services;
-
-import model.Employee;
-
-public interface IncomeCalculatorInterface {
-    public int calculateIncome(Employee employee);
-}
--- design pattern/SOLID((S-srb,O-ocb) princibles)/src/test/Main.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-public class Main {
-    public static void main(String[] args) {
-
-        Person person = Factory.getPerson("trainer");
-    }
-}
