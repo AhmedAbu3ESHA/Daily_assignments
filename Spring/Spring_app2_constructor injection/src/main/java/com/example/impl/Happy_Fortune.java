4e4453d53ecda999d08d035a3bb423eb232bce19
AhmedAbu3esha <38920018+AhmedAbu3ESHA@users.noreply.github.com>
2024-04-15 14:52:45 +0200

    Add files via upload

---
+++ Spring/Spring_app2_constructor injection/src/main/java/com/example/impl/Happy_Fortune.java
@@ -0,0 +1,9 @@
+package com.example.impl;
+import com.example.interfac.Fortun;
+public class Happy_Fortune  implements Fortun {
+	@Override
+	public String fortune() {
+		// TODO Auto-generated method stub
+		return	"happy";
+	}
+}
