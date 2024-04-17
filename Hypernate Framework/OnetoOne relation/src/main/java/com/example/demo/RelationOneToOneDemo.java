847762ca7764d850f0b46d6cf68d4c01026aef8f
AhmedAbu3esha <38920018+AhmedAbu3ESHA@users.noreply.github.com>
2024-04-17 11:54:02 +0200

    Add files via upload

---
+++ Hypernate Framework/OnetoOne relation/src/main/java/com/example/demo/RelationOneToOneDemo.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+
+import com.example.entities.Instructor;
+
+public class RelationOneToOneDemo {
+	
+	public static void main(String[] args) {
+		//create session factory
+		
+		org.hibernate.SessionFactory  sessionfactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class).buildSessionFactory();
+		
+		// open session
+		
+		Session session = sessionfactory.openSession(); 
+		
+		//begin transaction
+		session.beginTransaction();
+		// do my action on db
+	  Instructor inst=session.get(Instructor.class,1);
+	   System.out.println(inst);
+		
+		// commit transaction
+		
+		session.getTransaction().commit();
+		
+		//close session
+		session.close();
+		
+		sessionfactory.close();
+		
+
+	}
+
+}
