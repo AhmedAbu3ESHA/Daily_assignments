49a005ab22b286e72463a48f785f878bd1e33877
AhmedAbu3esha <38920018+AhmedAbu3ESHA@users.noreply.github.com>
2024-04-19 18:41:44 +0200

    Add files via upload

---
+++ Spring/Springcore injection ues xml/Spring_app1/src/main/java/com/example/impl/FootballCoach.java
@@ -0,0 +1,12 @@
+package com.example.impl;
+
+import com.example.interfac.Coach;
+
+public class FootballCoach implements Coach{
+	
+	public String getDailyWork() {
+	return	"Football";
+		
+	}
+
+}
