383988391124669d3ddcd01382edef776a9a565b
AhmedAbu3esha <38920018+AhmedAbu3ESHA@users.noreply.github.com>
2024-04-19 18:25:45 +0200

    Delete Spring directory

--- Spring/Springcore injection ues xml/Spring_app1/src/main/java/com/example/interfac/Coach.java
+++
@@ -1,6 +0,0 @@
-package com.example.interfac;
-
-public interface Coach {
-	public String getDailyWork();
-
-}
--- Spring/Springcore injection ues xml/Spring_app2_constructor injection/src/main/java/com/example/interfac/Fortun.java
+++
@@ -1,6 +0,0 @@
-package com.example.interfac;
-
-public interface Fortun {
-	public String fortune();
-
-}
