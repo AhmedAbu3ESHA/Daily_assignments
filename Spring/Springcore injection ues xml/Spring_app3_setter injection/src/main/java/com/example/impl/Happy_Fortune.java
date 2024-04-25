9ad47c842bf86bbaba5f3c20caf0bc838991d2ff
AhmedAbu3esha <38920018+AhmedAbu3ESHA@users.noreply.github.com>
2024-04-25 15:28:27 +0200

    Delete Spring/Springcore injection ues xml directory

--- Spring/Springcore injection ues xml/Spring_app3_setter injection/src/main/java/com/example/impl/Happy_Fortune.java
+++
@@ -1,9 +0,0 @@
-package com.example.impl;
-import com.example.interfac.Fortun;
-public class Happy_Fortune  implements Fortun {
-	@Override
-	public String fortune() {
-		// TODO Auto-generated method stub
-		return	"happy";
-	}
-}
